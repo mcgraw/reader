7043e44962390eebe5de425c922b39f2fc11b617
David McGraw <dlmcgraw07@gmail.com>
2015-06-17 18:37:53 -0500

    revisiting the database structure

--- models/sections.js
+++ models/sections.js
@@ -1,6 +1,19 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
+var LayoutType = {
+	CONTENT		: 'Content',
+	DOWNLOAD    : 'Download',
+	WALKTHROUGH : 'Walkthrough',
+	QUESTION    : 'Question',
+	CODE		: 'Code',
+	STEPS		: 'Steps',
+	IMAGE		: 'Image',
+	VIDEO		: 'Video',
+	REVIEW      : 'Review',
+	EMAIL		: 'Email'
+};
+
 var Section = new Schema( {
 	index: {
 		type: Number,
@@ -14,15 +27,15 @@ var Section = new Schema( {
 		type: String,
 		required: true
 	},
+	description: {
+		type: String,
+		required: true
+	},
 	rating: {
 		type: Number,
 		required: true,
 		default: 5
 	},
-	layout: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Layout'
-	}],
 	created: {
 		type: Date,
 		default: Date.now
@@ -30,7 +43,65 @@ var Section = new Schema( {
 	updated: {
 		type: Date,
 		default: Date.now
-	}
+	},
+	blocks: [{
+		style: {
+			type: String,
+			required: true,
+			default: LayoutType.CONTENT,
+			enum: [LayoutType.CONTENT, 
+				   LayoutType.DOWNLOAD,
+				   LayoutType.WALKTHROUGH,
+				   LayoutType.QUESTION,
+				   LayoutType.CODE,
+			   	   LayoutType.STEPS,
+			   	   LayoutType.IMAGE,
+			       LayoutType.VIDEO,
+			       LayoutType.REVIEW,
+				   LayoutType.EMAIL]
+		},
+		header: {
+			type: String
+		},
+		body: {
+			type: String
+		},
+		code: {
+			type: String
+		},
+		media: [{
+			type: String
+		}],
+		steps: [{
+			title: {
+				type: String
+			},
+			media_url: {
+				type: String
+			}
+		}],
+		questions: [{
+			answer: {
+				type: Number
+			},
+			items: [{
+				_id: { 
+					type: String 
+				},
+				title: {
+					type: String 
+				}
+			}]
+		}],
+		download: [{
+			extension: {
+				type: String
+			},
+			path: {
+				type: String
+			}
+		}]		
+	}]
 });
 
 exports.Section = Section;
---
+++ test/models/sections.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var SessionsDAO = require('../../sessions').SessionsDAO;
+var UsersDAO = require('../../users').UsersDAO;
+var ArticlesDAO = require('../../articles').ArticleDAO;
+	
+describe('Sections', function() {
+	
+	describe('Creating a new section', function() {
+		
+		// var user = new db.User({email: "david@xmcgraw.com", password: "pass1234"});
+		// var articles = new ArticlesDAO();
+			
+		// it('should create a new section', function(done) {
+		// 	done();
+		// });
+		
+		// it('should update section title', function(done) {
+		// 	done();
+		// });
+					
+	});
+	
+});
