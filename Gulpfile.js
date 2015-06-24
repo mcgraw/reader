3c3f25819a726089bc44f79d2444838076716ea5
David McGraw <dlmcgraw07@gmail.com>
2015-06-24 10:05:25 -0500

    finally have this runner in a good enough position

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,24 @@
 'use strict';
 
 var gulp 		= require('gulp');				// Workhorse
-var gutil 		= require('gulp-util');			// Log colors (gutils.log)
+// var gutil 		= require('gulp-util');			// Log colors (gutils.log)
 var jshint  	= require('gulp-jshint');		// Javascript code review
-var browserify	= require('gulp-browserify');	// Allows us to utilize the require() syntax in the front-end
+// var browserify	= require('gulp-browserify');	// Allows us to utilize the require() syntax in the front-end
 var concat		= require('gulp-concat');		// Concat files
 var stripDebug  = require('gulp-strip-debug');  //
 var uglify	    = require('gulp-uglify');		//
-var cache		= require('gulp-cache');		// 
-var livereload  = require('gulp-livereload');	// 
+// var cache		= require('gulp-cache');		// 
 var changed		= require('gulp-changed');		//
 var imagemin	= require('gulp-imagemin');		// 
 var minifyHTML  = require('gulp-minify-html');	//
 var autoprefix  = require('gulp-autoprefixer'); //
 var minifyCSS   = require('gulp-minify-css');   //
-var del			= require('del');
+var nodemon		= require('gulp-nodemon');		//
+var browserSync = require('browser-sync')		//
+// var del			= require('del');				//
 
 // JS hint task
-gulp.task('jshint', function() {
+gulp.task('lint', function() {
 	gulp.src('*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default')); // other 'pretty' reporters available
@@ -63,57 +64,42 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./build/styles/'));
 });
 
-// Default gulp task
-// gulp.task('default', ["imagemin", "htmlpage", "scripts", "styles"], function() {
-gulp.task('default', ['scripts'], function() {
+// Application state monitoring
+gulp.task('js-watch', ['lint'], browserSync.reload);
+gulp.task('serve', ['nodemon'], function() {	
 	
-	// watch for HTML changes
-	// gulp.watch('./src/*.html', ["htmlpage"]);
+	// Find more configuration options at http://www.browsersync.io/docs/options/
+	browserSync.init({
+		
+		// watch the following files
+		files: ['public/**/*.*'],
+		
+		// proxy our expressjs app running at this location
+		proxy: 'http://localhost:8080',
+		
+		// inform browser-sync to use the following port to avoid clashing
+		port: 4000,
+		
+		// open the proxied app in chrome
+		browser: ['google-chrome']
+		
+	});
 	
-	// watch for JS changes
-	gulp.watch('*.js', ['jshint', 'scripts']);
-	
-	// watch for CSS changes
-	// gulp.watch('./src/styles/*.css', ["styles"]);
-	
+	gulp.watch('*.js', ['js-watch']);
 });
 
-// // Watch
-// gulp.task('watch', function() {
+gulp.task('nodemon', function(cb) {
+	var called = false;
 	
-// 	// Watch .js files
-// 	gulp.watch('public/js/*.js', ['scripts']);
-	
-// 	// Create LiveReload server
-// 	livereload.listen();
-	
-// });
+	nodemon({
+		script: 'app.js',
+		watch: ['app.js']
+	})
+	.on('start', function onStart() {
+		if (!called) { cb(); }
+		called = true;
+	});
+});
 
-// // JSHint Task
-// gulp.task('lint', function() {
-// 	gulp.src('./*.js')
-// 		.pipe(jshint())
-// 		.pipe(jshint.reporter('default')); // other 'pretty' reporters available
-// });
-
-// // Browserify Task
-// gulp.task('browserify', function() {
-// 	// Single point of entry (make sure not to src ALL your files, browserify will figure it out for you)
-// 	gulp.src(['app.js'])
-// 	.pipe(browserify({
-// 		insertGlobals: true,
-// 		debug: true
-// 	}))
-// 	// Bundle to a single file
-// 	.pipe(concat('bundle.js'))
-// 	// Output it to our dist folder
-// 	.pipe(gulp.dest('public/js'));
-// });
-
-// gulp.task('watch', ['lint'], function() {
-// 	// Watch our scripts
-// 	gulp.watch(['*.js'],
-// 			   ['lint', 'browserify']);
-			   
-// 	gulp.watch(['*.html']);
-// });
+// ["imagemin", "htmlpage", "scripts", "styles"]
+gulp.task('default', ['serve']);
